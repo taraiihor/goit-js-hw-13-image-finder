fb13a4ec5578d9b6f3c3084371e63f3c2b776c12
taraiihor <63466237+taraiihor@users.noreply.github.com>
2020-11-15 17:21:17 +0200

    здача

--- src/index.js
+++ src/index.js
@@ -1,27 +1,49 @@
-import API from "./js/apiService";
-import galleryCard from "./templates/imege-gallery";
+import "./css/style.css";
+import galleryCard from "./templates/imege-gallery.hbs";
 import debouce from "lodash.debounce";
+import NewsApiService from "./js/apiService";
+import LoadMoreBtn from "./js/load-more-btn";
+
 const refs = {
   searchForm: document.querySelector("#search-form"),
   galleryImage: document.querySelector(".gallery"),
-  loadBtn: document.querySelector(".load-more-btn"),
 };
-
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+const newsApiServise = new NewsApiService();
 refs.searchForm.addEventListener("input", debouce(onSearsh, 500));
-
+loadMoreBtn.refs.button.addEventListener("click", onLoadMore);
 function onSearsh(e) {
   e.preventDefault();
-  clearRender();
-  const searchQuery = refs.searchForm.elements.query.value;
-  API.fetchConteiner(searchQuery)
-    .then(renderGallryCard)
-    .catch(error => error.log);
+  newsApiServise.query = e.target.value;
+
+  if (newsApiServise.query === "") {
+    clearRender();
+    loadMoreBtn.hide();
+    return;
+  }
+  loadMoreBtn.show();
+  loadMoreBtn.disable();
+  newsApiServise.resetPage();
+  newsApiServise.fetchArticle().then(artecles => {
+    clearRender();
+    renderGallryCard(artecles);
+    loadMoreBtn.enable();
+  });
 }
 
-function renderGallryCard(gallery) {
-  const markup = galleryCard(gallery);
-  refs.galleryImage.innerHTML = markup;
-  console.log(markup);
+function onLoadMore() {
+  loadMoreBtn.disable();
+  newsApiServise.fetchArticle().then(artecles => {
+    renderGallryCard(artecles);
+    loadMoreBtn.enable();
+  });
+}
+
+function renderGallryCard(articles) {
+  refs.galleryImage.insertAdjacentHTML("beforeend", galleryCard(articles));
 }
 
 function clearRender() {
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,9 +1,26 @@
-// const BASE_URL = "https://pixabay.com/api/";
-function fetchConteiner(searchQuery) {
-  return fetch(
-    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&per_page=12&key=8052974-676f52671a56653f7826cdc16`
-  ).then(response => {
-    return response.json();
-  });
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = "";
+    this.page = 1;
+  }
+  fetchArticle() {
+    const BASE_URL = "https://pixabay.com/api/";
+    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=8052974-676f52671a56653f7826cdc16`;
+    return fetch(url).then(response => {
+      this.incrementPage();
+      return response.json();
+    });
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
-export default { fetchConteiner };
